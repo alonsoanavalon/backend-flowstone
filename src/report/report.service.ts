256d67c6fb30f7e20946d529220595621791f8b5
k3yzen <alonso.anavalon@gmail.com>
2022-11-18 04:29:40 -0300

    posteando a maca

--- src/report/report.service.ts
+++ src/report/report.service.ts
@@ -1,10 +1,12 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import axios from 'axios';
 import { Model } from 'mongoose';
 import { ReportDTO } from './dto/report.dto';
 import { Report, ReportDocument } from './schemas/report.schema';
+import { AxiosResponse } from 'axios';
+import { Observable } from 'rxjs';
+
 
 @Injectable()
 export class ReportService {
@@ -21,14 +23,21 @@ export class ReportService {
         const createdReport = new this.reportModel(report);
         if (createdReport) {
             report['report_id'] = createdReport._id;
+            report['phone'] = "+56940340950";
+
+
             try {
-                const reportToArduino = this.httpService.post('https://5000-mojonapower-microservic-oullafk9vib.ws-us76.gitpod.io/sms', report);
-                console.log(reportToArduino)
+                const reportToArduino:Observable<AxiosResponse<any[]>> = await this.httpService.axiosRef.post('https://5000-mojonapower-microservic-oullafk9vib.ws-us76.gitpod.io/sms', report, {
+                    headers: {
+                        'Content-type': 'application/json'
+                    }
+                })
             } catch (err) {
                 console.log(err)
             }
-        }
+
         return createdReport.save();
+        }
     }
 
     async findAll(): Promise<Report[]> {
